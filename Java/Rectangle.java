7937ab85abcfe66f3f5b04608c8c06b3b7618253
natasyaav <natasyavirgichalia@natasyas-MacBook-Pro.local>
2016-10-10 01:42:28 -0400

    Add Rectangle

---
+++ Java/Rectangle.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import java.util.Optional;
+
+public class Rectangle extends Application{
+	@Override 
+	public void start(Stage primaryStage) {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setHeaderText("Input width and height of the rectangle, seperated by space");
+		Optional<String> input = dialog.showAndWait();
+		String [] side = input.get().split(" ");
+		double width  = Double.parseDouble(side[0]);
+		double height = Double.parseDouble(side[1]);
+		double perimeter = 2*width + 2*height;
+		double area = width * height;
+
+		Alert alert =  new Alert(AlertType.INFORMATION);
+		alert.setContentText("The perimeter of the rectangle is " + perimeter + "\nThe area of hte rectangle is " + area);
+		alert.showAndWait();
+	}
+}
